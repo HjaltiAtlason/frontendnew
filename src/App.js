812b52c6f9b4438663aaca216ac4172f60dab836
HjaltiAtlason <hjalti@jafna.is>
2019-09-11 08:27:03 +0000

    first commits

--- src/App.js
+++ src/App.js
@@ -1,26 +1,139 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { LinkContainer } from 'react-router-bootstrap';
+import { NavItem, Grid, Row, Col} from 'react-bootstrap';
+import { viewType } from './globals/utils';
+import ReceiveFromIsland from './login/components/ReceiveFromIsland';
+import { getUser, getIsLoggedIn } from './login/reducers/user';
+import LoginIsland from './login/components/LoginIsland';
+import MicroContainer from './login/components/MicroContainer';
+import LoginDev from './login/components/LoginDev';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit ss <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = ({
+  children,
+  company,
+  currentView,
+  dispatch,
+  user,
+  isLoggedIn
+}) => (
+  <div className={`jafna ${viewType(currentView.viewKey)}`}>
+    <nav className="navbar navbar-transparent navbar-fixed-top navbar-color-on-scroll">
+      <div className="container">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle"
+            data-toggle="collapse"
+            data-target="#navigation-example"
+          >
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+          </button>
+          <a href="/">
+            <div className="logo-container">
+              <div className="logo">
+                <img alt="Logo" src="bootstrap/img/logo.jpg" />
+              </div>              
+            </div>
+            <div className="brand">Jafna ehf</div>
+          </a>
+        </div>
+        <div className="collapse navbar-collapse" id="navigation-example">
+          <ul className="nav navbar-nav navbar-right">
+            <LinkContainer to="/Lantaki">
+              <NavItem eventKey={1}>Lántaki</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/Fjarfestir">
+              <NavItem eventKey={2}>Fjárfestir</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/Fyrirtaekid">
+              <NavItem eventKey={3}>Fyrirtækið</NavItem>
+            </LinkContainer>
+            <li>
+              {user.token ? (<p> Velkomin(n) {user.firstName} </p>) : null}
+            </li>
+          </ul>
+        </div>
+
+      </div>
+
+    </nav>
+
+    <div className="wrapper">
+      {user.email ? (
+        <div className="header header-filter city-background" />
+      ) : null}
+      {!isLoggedIn ? (
+        <Grid>
+          <Row className="show-grid">
+            <Col xs={12} md={6}>
+              <MicroContainer header="Innskráning Island.is (nota https://www.jafna.is/)">                      
+                <LoginIsland dispatch={dispatch} isBorrower />          
+              </MicroContainer>
+            </Col>
+            <Col xs={12} md={6}>
+              <MicroContainer header="Innskráning Island.is (nota https://www.jafna.is/)">          
+                <LoginIsland dispatch={dispatch} isBorrower={false} />        
+              </MicroContainer>
+            </Col>
+          </Row>
+        </Grid>
+      ) : null}
+      {children}
+      <footer className="footer">
+        <div className="container">
+          <nav className="pull-left">
+            <ul>
+              <li>
+                <a> Um okkur </a>
+              </li>
+              <li>
+                <a> Spurt og svarað</a>
+              </li>
+              <li>
+                <a>Blogg</a>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="copyright pull-right">
+            {company.address} Sími {company.phone} &copy; {company.name}.
+          </div>
+        </div>
+        <LoginDev dispatch={dispatch} />
+        <ReceiveFromIsland dispatch={dispatch} />
+      </footer>
     </div>
-  );
-}
+  </div>
+);
 
-export default App;
+App.propTypes = {
+  currentView: PropTypes.object,
+  children: PropTypes.element,
+  company: PropTypes.object,
+  dispatch: PropTypes.func,
+  user: PropTypes.object,
+  isLoggedIn: PropTypes.bool
+};
+
+const mapStateToProps = (state) => ({
+  company: state.login.company,
+  currentView: state.login.currentView,
+  user: getUser(state),
+  isLoggedIn: getIsLoggedIn(state),
+  // loan: getLoan(state),
+  account: state.apply.account,
+  // payments: getPayments(state),
+  notes: state.invest.notes
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatch
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
+import InvestorScreen from './invest/InvestorScreen';
+import ApplicantScreen from './apply/ApplicantScreen';
+import AboutJafna from './AboutJafna';
+import document from './globals/document';
+import firebase from 'firebase/app'
+import 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const config = {
+  apiKey: 'AIzaSyB1TsfIfEbKHEtCNaU6BMjC4R6ykyJb01w',
+  authDomain: 'jafnafiles.firebaseapp.com',
+  databaseURL: 'https://jafnafiles.firebaseio.com',
+  projectId: 'jafnafiles',
+  storageBucket: 'jafnafiles.appspot.com',
+  messagingSenderId: '702170483690'
+};
+firebase.initializeApp(config);
 
+render(  
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <Route path="/Lantaki" component={ApplicantScreen} />
+        <Route path="/Fjarfestir" component={InvestorScreen} />
+        <Route path="/Fyrirtaekid" component={AboutJafna} />
+        <Route path="*" component={ApplicantScreen} />
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
